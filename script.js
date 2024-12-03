08fcdc94358af81f81982b738e45dad3ea2ebeae
lakshay3452 <ug13262@gmail.com>
2024-12-03 15:46:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".typing", {
+        strings: ["", "Web Designer", "Web Developer", "Graphic Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
